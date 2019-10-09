d1c1387efab96b99e6b9a2d97bdf74277ab672c6
Daniele Esposti <daniele.esposti@revolut.com>
2019-10-09 22:22:18 +0100

    Fixed README in setup

--- setup.py
+++ setup.py
@@ -1,12 +1,15 @@
-from setuptools import setup
+from pathlib import Path
 
+from setuptools import setup
 from setuptools_rust import Binding, RustExtension
 
 setup(
     name="hephaestus",
-    version="0.1.0",
+    version="0.1.1",
     rust_extensions=[RustExtension("hephaestus.hephaestus", binding=Binding.PyO3)],
     packages=["hephaestus"],
     # rust extensions are not zip safe, just like C-extensions.
     zip_safe=False,
+    long_description=Path("README.md").read_text(),
+    long_description_content_type="text/markdown",
 )

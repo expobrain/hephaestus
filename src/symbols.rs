bb09758909c012d6509fa8302635613db787df1d
Daniele Esposti <daniele.esposti@revolut.com>
2019-10-31 11:11:30 +0000

    Fixed disclaimers

--- src/symbols.rs
+++ src/symbols.rs
@@ -1,6 +1,7 @@
 // MIT License
 //
 // Copyright (c) 2018 Hans-Martin Will
+// Copyright (c) 2019 Daniele Esposti
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
